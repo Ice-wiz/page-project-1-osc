b9b8f1266f82876f184a399d07274ea54146ebd8
Aryan Sharma <aryansharma@WINDELLAP-733.local>
2024-12-05 05:11:20 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { VWOScript } from "nextjs-smartcode-injector";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <VWOScript 
+        accountId="955578"
+        type="OSC"
+        settingsTimeout={2000}
+        hideElement="body"
+        hideElementStyle="opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;transition:none !important;"
+        scriptAttributes={{
+          strategy: "beforeInteractive"
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
